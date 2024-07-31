d7a960a0954f4cdc2b6c6767c791fec4e6004bfc
kamishettimahesh <maheshkamishetti15@gmail.com>
2024-08-01 01:23:09 +0530

    app_alb

--- main.tf
+++ main.tf
@@ -13,18 +13,20 @@ resource "aws_lb" "app_alb" {
   )
 }
 
-resource "aws_lb_target_group" "app_tg" {
+resource "aws_lb_target_group" "app-tg" {
   name     = "app-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_ssm_parameter.vpc_id.value
+  target_type = "instance"
   deregistration_delay = 60
+
   health_check {
       healthy_threshold   = 2
       interval            = 10
       unhealthy_threshold = 3
       timeout             = 5
-      path                = "/health"
+      path                = "/app-tg"
       port                = 80
       matcher = "200-299"
   }
@@ -39,12 +41,12 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
+    target_group_arn = aws_lb_target_group.app-tg.arn
   }
 }
 
 resource "aws_lb_target_group_attachment" "app_instance" {
-  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_group_arn = aws_lb_target_group.app-tg.arn
   target_id        = data.aws_ssm_parameter.instance_id.value
   port             = 80
 }
